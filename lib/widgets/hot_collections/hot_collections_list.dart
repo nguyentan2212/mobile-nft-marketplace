9703c24d62bcbbaaf29ca2ace88f9ebdb02e7555
nguyentan2212 <nguyentan2212@gmail.com>
2022-05-05 18:13:46 +0700

    trending collections list

--- lib/widgets/hot_collections/hot_collections_list.dart
+++ lib/widgets/hot_collections/hot_collections_list.dart
@@ -2,8 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:mobile_nft_marketplace/models/collection.dart';
 import 'package:mobile_nft_marketplace/widgets/hot_collections/hot_collection_card.dart';
 
-class HotCollectionList extends StatelessWidget {
-  const HotCollectionList({Key? key}) : super(key: key);
+class HotCollectionsList extends StatefulWidget {
+  const HotCollectionsList({Key? key}) : super(key: key);
+
+  @override
+  State<HotCollectionsList> createState() => _HotCollectionsListState();
+}
+
+class _HotCollectionsListState extends State<HotCollectionsList> {
+  final PageController pageController = PageController(viewportFraction: 0.5);
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageController.addListener(
+      () {
+        int position = pageController.page!.round();
+
+        if (currentPage != position) {
+          {
+            setState(() {
+              currentPage = position;
+            });
+          }
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +38,18 @@ class HotCollectionList extends StatelessWidget {
     for (int i = 0; i < hotCollections.length; i++) {
       collections.add(
         HotCollectionCard(
-            name: hotCollections[i].name, image: hotCollections[i].image, nftType: hotCollections[i].nftType),
+          collection: hotCollections[i],
+        ),
       );
     }
-
-    return Row(
-      children: collections,
+    return SizedBox(
+      height: 180,
+      child: PageView(
+        controller: pageController,
+        padEnds: false,
+        physics: const BouncingScrollPhysics(),
+        children: collections,
+      ),
     );
   }
 }
