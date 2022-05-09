6563dfc44bb4eeb9da39054ac98a8bd8078148ec
nguyentan2212 <nguyentan2212@gmail.com>
2022-05-09 15:48:37 +0700

    add sliver app bar

--- lib/screens/nft_detail_screen.dart
+++ lib/screens/nft_detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_nft_marketplace/widgets/sliver_widgets/sliver_app_bar_with_image.dart';
 import 'package:provider/provider.dart';
 import 'package:mobile_nft_marketplace/app_state.dart';
 
@@ -9,19 +11,50 @@ class NFTDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var state = context.read<AppState>();
 
-    return Center(
-      child: GestureDetector(
-        onTap: (() => Navigator.pop(context)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('NFT Detail Screen'),
-            Text(state.selectedItem!.name),
-            Text(
-              state.selectedItem!.price.toString(),
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBarWithImage(
+            image: Image.asset("assets/images/items/${state.selectedItem!.image}"),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 10,
             ),
-          ],
-        ),
+          ),
+          const SliverAppBar(
+            pinned: true,
+            expandedHeight: 160.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('SliverAppBar'),
+              background: FlutterLogo(),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('${index + 10}', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
