0212cdc0876aebf3e874709f50e391714d65cfd2
nguyentan2212 <nguyentan2212@gmail.com>
2022-05-29 16:10:25 +0700

    feat: add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_nft_marketplace/app_state.dart';
 import 'package:mobile_nft_marketplace/screens/home/home_screen.dart';
+import 'package:mobile_nft_marketplace/screens/live_bidding/live_bidding_screen.dart';
 import 'package:mobile_nft_marketplace/utils/colors.dart';
-import 'package:mobile_nft_marketplace/widgets/bottom_bar.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -28,16 +28,13 @@ class _MyAppState extends State<MyApp> {
           primaryColor: purple[900],
           backgroundColor: black,
         ),
-        home: SafeArea(
-          child: Consumer<AppState>(
-            builder: (context, state, child) {
-              return const Scaffold(
-                body: HomeScreen(),
-                bottomNavigationBar: BottomBar(),
-              );
-            },
-          ),
-        ),
+        initialRoute: '/',
+        routes: {
+          // When navigating to the "/" route, build the FirstScreen widget.
+          '/': (context) => const HomeScreen(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+          '/liveBidding': (context) => const LiveBiddingScreen(),
+        },
       ),
     );
   }
